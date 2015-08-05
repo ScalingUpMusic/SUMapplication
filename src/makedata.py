64302fa0be7c76c4254b6a34dce649cc8617539c
Ross Boberg <ross.boberg@gmail.com>
2015-08-05 16:52:46 -0700

    program make cleaner data

---
+++ src/makedata.py
@@ -0,0 +1,58 @@
+import os
+import h5py
+import numpy
+import string
+
+sc = SparkContext(appName='H5 2 Tuples')
+
+def h52tuple(path, data_i=[3,4,21,22,23,24,25,26,27,28,29]):
+	#3: duration
+	#4: end of fade in
+	#21: key
+	#22: key confidence
+	#23: loudness
+	#24: mode
+	#25: mode_confidence
+	#26: start_of_fade_out
+	#27: tempo
+	#28: time_signature
+	#29: time_signature_confidence
+	#30: track id
+	song_analysis = h5py.File(path, 'r')['analysis']['songs'][0]
+	data = [song_analysis[i] for i in data_i]
+	track_id = song_analysis[30]
+	tags = list(h5py.File(path, 'r')['musicbrainz']['artist_mbtags'][:])
+	return (track_id, (tags, data))
+
+
+def makeDirList(base_dir, depth=1):
+	dir_list = []
+	alpha = list(string.ascii_uppercase)
+	if depth > 0:
+		for a in alpha:
+			fdir = base_dir + a + '/'
+			dir_list.extend(makeDirList(fdir, depth = depth-1))
+		return dir_list
+	else:
+		return [base_dir]
+
+def main(argv):
+	fdir_base = '/gpfs/A/A/'
+	dir_list = sc.parallelize(makeDirList(fdir_base))
+
+	file_list = dir_list.flatMap(lambda fdir: [fdir + '/' + f for f in os.listdir(fdir)])
+	file_list.collect()
+
+	tagdata = file_list.map(lambda fpath: h52tuple(fpath))
+	tagdata.collect()
+
+	tagdata.saveAsTextFile('/root/data/summary')
+
+	### ambari
+	#salt on each machine 
+	#add slat master to host file
+	#control cluster from salt master
+	#start ambari from salt master
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
